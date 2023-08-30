3c87f48e654272e9fae3fbc29ca009f3a76bca9b
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-08-30 19:43:30 +0530

    home-page-done

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-
 window.addEventListener("scroll", function() {
     if(this.scrollY > 10){
         $(".header").addClass("sticks");
@@ -17,6 +16,33 @@ $(document).ready(function(){
         $(".overlay").toggleClass("active");
     });
 })
+
+// Counter start
+const counterSection = document.querySelector(".counters");
+const counts = document.querySelectorAll(".count");
+if(counts != null && counterSection != null){
+    let counterObserver = new IntersectionObserver((entries, observer)=>{
+        let [entry] = entries;
+        if(!entry.isIntersecting) return;
+        let speed = 200;
+        counts.forEach((count,index)=>{
+            const updateCounts =()=>{
+                let targetNumber = +count.dataset.target;
+                let initialValue = +count.innerText;
+                let incPerCount = targetNumber / speed;
+                if(initialValue < targetNumber){
+                    count.innerText = Math.ceil(initialValue + incPerCount);
+                    setTimeout(updateCounts, 20);
+                }
+            }
+            updateCounts();
+        });
+        observer.unobserve(counterSection);
+    });
+    counterObserver.observe(counterSection);
+}
+// Counter end
+
 
 const swiper = new Swiper(".swiper-container", {
     slidePerView:1,
